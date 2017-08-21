710fdab94ec697dca78449124d5aaa75f47b6270
AlexandreRech <alexandre.rech@hotmail.com>
2017-08-21 20:58:23 -0300

    Corrigido o commit

---
+++ Projetos/Arquitetura/eAgenda.ConsoleApp/TarefaInvalidaException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eAgenda.ConsoleApp
+{
+    public class TarefaInvalidaException : ApplicationException
+    {
+        public TarefaInvalidaException(string msg) : base(msg)
+        {
+
+        }
+       
+
+        public TarefaInvalidaException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+       
+    }
+}
